891dcefb9f79823ec0e1ac7520c440704e7654e8
elnurra <elnurra@code.edu.az>
2022-08-02 14:07:48 +0400

    C# tasks

---
+++ TaskForAmountDaysInMonths/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TaskForAmountDaysInMonths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 28,b=30,c=31;           
+            string month = "September";
+            switch (month)
+            {
+                case "February":
+                    Console.WriteLine(a);
+                 break;
+                case "April":
+                    Console.WriteLine(b);
+                 break;
+                case "June":
+                    Console.WriteLine(b);
+                    break;
+                case "September":
+                    Console.WriteLine(b);
+                    break;
+                case "November":
+                    Console.WriteLine(b);
+                    break;
+                case "January":
+                    Console.WriteLine(c);
+                    break;
+                case "March":
+                    Console.WriteLine(c);
+                    break;
+                case "May":
+                    Console.WriteLine(c);
+                    break;
+                case "Jule":
+                    Console.WriteLine(c);
+                    break;
+                case "August":
+                    Console.WriteLine(c);
+                    break;
+                case "December":
+                    Console.WriteLine(c);
+                    break;
+                default:
+                    Console.WriteLine("Error input is wrong");
+                    break;
+
+
+
+            }
+        }
+    }
+}
---
+++ TaskForNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TaskForNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                                           
+            int n = 17, m = 100;                   
+            for (n=17; n <m; n++)                   
+            {
+                if (n % 7 == 0) 
+                {
+                    Console.WriteLine(n);
+                }
+
+
+
+            }
+        
+        }
+    }
+}
